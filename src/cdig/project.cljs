23a6381bef318609c3b3fbbf0ae96c82ccdb4701
Ivan Reese <ivanreese@gmail.com>
2017-05-23 17:58:01 -0600

    Don't pull the gulpfile

--- src/cdig/project.cljs
+++ src/cdig/project.cljs
@@ -5,7 +5,7 @@
 
 (declare pull)
 
-(def system-files [".gitignore" "cdig.json" "gulpfile.coffee" "package.json"])
+(def system-files [".gitignore" "cdig.json" "package.json"])
 (def generated-files [".DS_Store" ".git" "bower.json" "bower_components" "deploy" "node_modules" "public" "yarn.lock"])
 (def new-project-files {:cd-module ["source/index.kit" "source/pages/ending.kit" "source/pages/title.kit" "source/styles/fonts.scss"]
                         :svga ["source/symbol.coffee" "source/config.coffee"]})
